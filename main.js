045356486fc791fec0b6bdc87b3b76394b7067be
sandy0903 <44163875+sandy0903@users.noreply.github.com>
2021-11-26 10:56:44 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector(".hamburger-menu").onclick=function(){
+    if( this.classList.contains("animate")){
+        this.classList.remove("animate")
+        document.querySelector(".content").classList.remove("animate-col")
+    }else{
+        this.classList.add("animate");
+        document.querySelector(".content").classList.add("animate-col")
+    }
+    
+}
